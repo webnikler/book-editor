575d47998f53d5edc06a3b481cb7a9cdfcd3c7f3
Nikita Petrov <webnikler@gmail.com>
2019-10-03 23:47:54 +0300

    feat: add books store and create books table

--- src/pages/Books.vue
+++ src/pages/Books.vue
@@ -1,20 +1,60 @@
 <template>
-  <ul>
-    <li>1</li>
-  </ul>
+  <div class="container">
+    <div class="row">
+      <div class="table-responsive">
+        <table class="table table-bordered table-hover table-sm">
+          <thead class="thead-dark">
+            <tr>
+              <th v-for="(title, i) of columnsMap.values()" :key="i">{{ title }}</th>
+            </tr>
+          </thead>
+          <tbody>
+          <tr v-for="book of booksState.books" :key="book.id">
+            <td v-for="key in columnsMap.keys()" :key="`${book.id}${key}`">{{ book[key] }}</td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    </div>
 </template>
 
 <script>
 
 import { Component, Vue } from 'vue-property-decorator';
+import { Action, State } from 'vuex-class';
+import { name as namespace } from '../store/books';
 
 @Component
 export default class BooksPage extends Vue {
-  books = [];
+  @State(namespace) booksState;
+
+  @Action('loadBooks', { namespace }) loadBooks;
+
+  columnsMap = new Map();
+
+  constructor() {
+    super();
+
+    this.columnsMap.set('imageUrl', 'Изображение');
+    this.columnsMap.set('title', 'Заголовок');
+    this.columnsMap.set('firstName', 'Имя автора');
+    this.columnsMap.set('secondName', 'Фамилия автора');
+    this.columnsMap.set('pagesCount', 'Количество страниц');
+    this.columnsMap.set('publisherName', 'Название издательства');
+    this.columnsMap.set('publicationYear', 'Год публикации');
+    this.columnsMap.set('releaseDate', 'Дата выхода в тираж');
+  }
+
+  mounted() {
+    this.loadBooks();
+  }
 }
 
 </script>
 
 <style scoped lang="scss">
-
+  .table * {
+    font-size: 14px;
+  }
 </style>
---
+++ src/store/books.js
@@ -0,0 +1,81 @@
+import {
+  Module,
+  VuexModule,
+  Action,
+  Mutation,
+} from 'vuex-module-decorators';
+import store from '../store';
+import booksMock from '../mocks/books.json';
+
+export const name = 'usersStore';
+
+@Module({
+  namespaced: true,
+  dynamic: true,
+  name,
+  store,
+})
+export default class BooksStore extends VuexModule {
+  books = [];
+
+  booksLoading = false;
+
+  getBooks() {
+    return this.books;
+  }
+
+  getBookById(bookId) {
+    return this.books.find(({ id }) => id === bookId);
+  }
+
+  getBookIndex(book) {
+    return this.books.findIndex(({ id }) => book.id === id);
+  }
+
+  @Action({ commit: 'pushBook' }) addBook(book) {
+    return book;
+  }
+
+  @Action({ commit: 'putBook' }) updateBook(book) {
+    return [book, this.getBookIndex(book)];
+  }
+
+  @Action({ commit: 'removeBookByIndex' }) removeBook(book) {
+    return this.getBookIndex(book);
+  }
+
+  @Action loadBooks() {
+    this.context.commit('startBooksLoad');
+
+    return new Promise(resolve => setTimeout(resolve, 1000))
+      .then(() => {
+        this.context.commit('setBooks', booksMock);
+        return booksMock;
+      })
+      .finally(() => this.context.commit('endBooksLoad'));
+  }
+
+  @Mutation setBooks(books) {
+    this.books = books;
+  }
+
+  @Mutation putBook([book, index]) {
+    this.books[index] = book;
+  }
+
+  @Mutation pushBook(book) {
+    this.books.push(book);
+  }
+
+  @Mutation removeBookByIndex(index) {
+    this.books.splice(index, 1);
+  }
+
+  @Mutation startBooksLoad() {
+    this.booksLoading = true;
+  }
+
+  @Mutation endBooksLoad() {
+    this.booksLoading = false;
+  }
+}
